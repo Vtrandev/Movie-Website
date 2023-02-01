912c698a0c76ba4836c0a91eaade20a02ce54600
Von <xvtran@hotmail.com>
2023-02-01 12:57:48 -0800

    search function implemented

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-//https://api.smartcar.com/v2.0/vehicles
+const movieListEl = document.querySelector('.movie-list')
+const search = localStorage.getItem('search');
 
-async function getCars() {
-    const promise = await fetch('https://javascript-sdk.smartcar.com/v2/redirect?app_origin=https://myapp.com')
-    const result = await promise.json();
-
-    console.log(result);
+function onSearchChange(event) {
+    const search = event.target.value;
+    getMovies(search);
 }
 
-getCars();
+
+async function getMovies(search) {
+    const promise = await fetch(`http://www.omdbapi.com/?apikey=aafd31ec&s=${search}`)
+    // const promise = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const result = await promise.json();
+
+    movieListEl.innerHTML = result.Search.map((elem) => movieHTML(elem))
+}
+
+function movieHTML(movie) {
+    return `<div class="movie-list__wrapper">
+    <figure>
+        <img src="${movie.Poster}"
+            alt="">
+    </figure>
+    <h1>Title: ${movie.Title}</h1>
+    <p>Year: ${movie.Year}</p>
+    <p>ID: ${movie.imdbID}</p>
+    <p>Type: ${movie.Type}</p>
+    </div>`
+}
+
+getMovies('hello');
